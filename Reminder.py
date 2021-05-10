9ae9e86d7fa5fae237e76943782c49b2b3673d1d
Anita kumari jena <64706351+anitakumarijena@users.noreply.github.com>
2021-05-10 23:21:32 +0530

    Simple Reminder application

---
+++ Reminder.py
@@ -0,0 +1,8 @@
+import time
+print("What shall I remind you about?")
+text = str(input())
+print("In how many minutes?")
+local_time =  float(input())
+loacal_time =  lacal_time * 60
+time.sleep(local_time)
+print(text)
